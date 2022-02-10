5f7d92aeb3d61e2b3e47f121d6b0f88e8585873a
LoisKOUNINEF <lkouninef@gmail.com>
2022-02-10 14:31:41 +0100

    Add Cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_132928) do
+ActiveRecord::Schema.define(version: 2022_02_10_133047) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2022_02_10_132928) do
     t.datetime "updated_at", null: false
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "doctors", force: :cascade do |t|
