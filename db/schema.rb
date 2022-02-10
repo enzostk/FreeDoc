2df0c4486c0f93800f85284ee7ba847cd66eae78
LoisKOUNINEF <lkouninef@gmail.com>
2022-02-10 14:30:33 +0100

    Add Appointments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_132804) do
+ActiveRecord::Schema.define(version: 2022_02_10_132928) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "date"
+    t.integer "doctor_id"
+    t.integer "patient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string "first_name"
