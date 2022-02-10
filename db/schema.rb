3ef09ca07c52080d9724031411030ed0fe1d382c
LoisKOUNINEF <lkouninef@gmail.com>
2022-02-10 14:35:58 +0100

    Add Specialties

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_133339) do
+ActiveRecord::Schema.define(version: 2022_02_10_133431) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2022_02_10_133339) do
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "specialties", force: :cascade do |t|
+    t.string "specialty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
